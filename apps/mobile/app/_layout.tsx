30158cce93408ab851cb9b344acff1148e874eca
Kacper Zabłudowski <zabluka_aehit@students.vizja.pl>
2026-03-10 21:27:59 +0100

    feat(mobile): update navigation structure with tab layout and integrate react-native-screens version 4.16.0

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,22 +1,9 @@
-import { Slot } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import { TRPCProvider } from "../utils/provider";
+import { Stack } from "expo-router";
 
-export default function RootLayout() {
+export default function Layout() {
 	return (
-		<TRPCProvider>
-			<View style={styles.container}>
-				<Slot />
-				<StatusBar style="auto" />
-			</View>
-		</TRPCProvider>
+		<Stack>
+			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+		</Stack>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#f7f7f2",
-	},
-});
