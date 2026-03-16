8a12a9215f167476885f289be625185bcdc27293
Kacper Zabłudowski <zabluka_aehit@students.vizja.pl>
2026-03-16 20:45:39 +0100

    fix(mobile): update router path in SocialSignIn component for correct navigation

--- apps/mobile/app/sign-in.tsx
+++ apps/mobile/app/sign-in.tsx
@@ -16,7 +16,7 @@ export default function SocialSignIn() {
 				);
 				return;
 			}
-			router.replace("mobile://");
+			router.replace("/");
 		} catch (error) {
 			const message = error instanceof Error ? error.message : "Unknown error";
 			console.error("LOGIN EXCEPTION", error);
