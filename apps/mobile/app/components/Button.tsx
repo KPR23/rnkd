2bc07df190d0802ed012d84e1ad8026bd04c02a6
Kacper Zabłudowski <zabluka_aehit@students.vizja.pl>
2026-03-17 10:46:48 +0100

    feat(mobile): update app configuration and add NotFound screen, enhance Button and IconButton components

--- apps/mobile/app/components/Button.tsx
+++ apps/mobile/app/components/Button.tsx
@@ -1,17 +1,15 @@
 import { Pressable, Text } from "react-native";
 
-export default function Button({
-	variant,
-	className,
-	actionText,
-}: {
+interface Props {
 	variant: "primary" | "secondary";
 	className?: string;
 	actionText: string;
-}) {
+}
+
+export default function Button({ variant, className, actionText }: Props) {
 	return (
 		<Pressable
-			className={`p-2 h-11 items-center justify-center active:opacity-70 ${variant === "primary" ? "bg-primary" : "bg-dark border border-gray"} ${className}`}
+			className={`will-change-pressable p-2 h-11 items-center justify-center active:opacity-70 ${variant === "primary" ? "bg-primary" : "bg-dark border border-gray"} ${className}`}
 		>
 			<Text
 				className="text-text uppercase text-sm"
--- apps/mobile/app/components/IconButton.tsx
+++ apps/mobile/app/components/IconButton.tsx
@@ -17,7 +17,7 @@ export default function IconButton({
 	return (
 		<Pressable
 			{...pressableProps}
-			className={`items-center justify-center border border-dark active:opacity-70 ${className}`}
+			className={`will-change-pressable items-center justify-center border border-dark active:opacity-70 ${className}`}
 			style={[{ width: size, height: size }]}
 		>
 			{icon}
