73529307399b58f193384d59900aff099b5c7b94
Kacper Zabłudowski <zabluka_aehit@students.vizja.pl>
2026-03-24 19:28:58 +0100

    refactor(profile): remove unused import of ListMatchHistory component from ProfilePage

--- apps/web/src/app/profile/page.tsx
+++ apps/web/src/app/profile/page.tsx
@@ -1,7 +1,6 @@
 import { auth } from "@/src/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-import { ListMatchHistory } from "./_components/listMatchHistory";
 
 export default async function ProfilePage() {
 	const session = await auth.api.getSession({
