510a0e03fc31844b710a47ebf76d144583903a6e
Kacper Zabłudowski <zabluka_aehit@students.vizja.pl>
2026-05-26 21:59:44 +0200

    fix(components): update RSBadge to utilize color prop for improved styling consistency and enhance text appearance

--- apps/mobile/src/components/RSBadge.tsx
+++ apps/mobile/src/components/RSBadge.tsx
@@ -1,5 +1,6 @@
 import { View } from "react-native";
 
+import { colors } from "@repo/ui/colors";
 import AppText from "@/src/components/AppText";
 
 type Props = {
@@ -9,15 +10,13 @@ type Props = {
 export default function RSBadge({ globalRs }: Props) {
   return (
     <View className="bg-card border-muted h-7.5 flex-row items-center justify-center gap-0.75 rounded-full border px-2">
-      <AppText
-        className="text-text text-base font-medium tracking-[-0.5px]"
-        weight="medium"
-      >
+      <AppText className="text-base tracking-[-0.5px]" weight="medium">
         {globalRs}
       </AppText>
       <AppText
-        className="text-primary text-sm leading-px font-medium"
+        className="text-sm leading-px"
         weight="medium"
+        color={colors.primary}
       >
         RS
       </AppText>
